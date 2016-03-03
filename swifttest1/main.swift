f3691bd5191457c4e7c68b74f4ffdcfa6e8d266c
wangyufei993 <1531481086@qq.com>
2016-03-03 11:26:01 +0800

    chance1
    …

---
+++ swifttest1/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swifttest1
+//
+//  Created by 20131105755 on 16/3/3.
+//  Copyright © 2016年 20131105755. All rights reserved.
+//
+
+import Foundation
+var a:Int,b:Int,c:Int
+a=100
+b=200
+c=a+b
+print("\(c)")
+
