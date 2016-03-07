d1c605b297686d5e39a5e812eca0f1c378a33b7a
wangyufei993 <1531481086@qq.com>
2016-03-07 21:12:58 +0800

    change5
    函数

--- swifttest1/main.swift
+++ swifttest1/main.swift
@@ -11,8 +11,29 @@ var a:Int,b:Int,c:Int
 a=100
 b=200
 c=a+b
+print("\(a)+\(b)=\(c)")
+
+
 for ch in "hello".characters{
 print(ch)
 }
-print("\(a)+\(b)=\(c)")
+
+
+func hello() {
+print("hello")
+}
+hello()
+
+
+func add1(x:Int,y:Int,z:Int) {
+    print("\(x)+\(y)+\(z)=\(x+y+z)")
+}
+add1(10,y:20,z: 30)
+
+
+func add2(x:Int,_ y:Int,_ z:Int) {
+    print("\(x)+\(y)+\(z)=\(x+y+z)")
+}
+add2(10,20,30)
+
 
